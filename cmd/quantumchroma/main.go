abd74345d033244e6f8a3d66a78792291e97c70c
Ludo <97764715+ludo53@users.noreply.github.com>
2025-08-02 20:03:35 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/quantumchroma/main.go
@@ -0,0 +1,23 @@
+// cmd/quantumchroma/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"quantumchroma/internal/quantumchroma"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := quantumchroma.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
